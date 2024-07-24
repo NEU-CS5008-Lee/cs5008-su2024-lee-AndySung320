5db52646c27c3b5584e452529ed6d8b3ba724fc0
AndySung0320 <andysung0320@gmail.com>
2024-07-24 14:33:14 -0700

    1st upload of hw11c

--- assignment11/hw11c.c
+++ assignment11/hw11c.c
@@ -1,5 +1,5 @@
-// name: <your name here>
-// email: <your email here>
+// name: Di-En Sung
+// email: sung.di@northeastern.edu.tw
 
 #include <stdio.h>
 
@@ -8,7 +8,27 @@ int d[20];
 long long int dp(int n) {
 
     // Add your code here
-
+    if(n % 2 != 0){
+        return 0;
+    }
+    if(n == 0){
+        return 1;
+    }
+    if (n == 2){
+        return 3;
+    }
+    for(int i=0; i<=n; i++){
+        d[i] = 0;
+    }
+    d[0] = 1;
+    d[2] = 3;
+    for(int i=4; i<=n; i+=2){
+        d[i] = 3*d[i-2];
+        for(int j = 4; j<=i; j+=2){
+            d[i] += 2*d[i-j];
+        }
+    }
+    return d[n];
 }
 
 int main(void) {
